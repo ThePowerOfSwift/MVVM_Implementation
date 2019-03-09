54f3a0bf82c95c9305e8efb959f8f8234db67239
Dmitriy Lupych <dmitriy.lupych@softermii.com>
2019-03-09 21:10:28 +0200

    Initial Commit

---
+++ MVVM_Implementation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_Implementation
+//
+//  Created by Дмитрий on 3/9/19.
+//  Copyright © 2019 Dmitry Lupich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
